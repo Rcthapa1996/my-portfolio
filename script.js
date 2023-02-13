f47a1f2051a3e71318671e84d67d3f7a76aa0f75
Rcthapa1996 <Rcthapa1996@gmail.com>
2023-02-13 11:15:22 +0545

    about and header

---
+++ script.js
@@ -0,0 +1,23 @@
+const cv = document.getElementById("cv");
+const portfolio = document.getElementById("portfolio");
+const homeControler = document.getElementById("controler");
+const homeSection = cv.parentElement;
+
+let showCv = false;
+
+function toggelHome() {
+  if (showCv) {
+    homeSection.style.backgroundImage = "url('./img/img_bg_1.jpg')";
+    cv.style.display = "flex";
+    portfolio.style.display = "none";
+  } else {
+    homeSection.style.backgroundImage = "url('./img/img_bg_2.jpg')";
+    cv.style.display = "none";
+    portfolio.style.display = "flex";
+  }
+  homeControler.children[0].classList.toggle("active");
+  homeControler.children[1].classList.toggle("active");
+  showCv = !showCv;
+}
+
+setInterval(toggelHome, 5000);
